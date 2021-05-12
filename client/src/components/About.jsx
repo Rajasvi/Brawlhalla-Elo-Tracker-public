8d5e0b4f5190514dd801e7508182e9e6d28013f5
Rajasvi <rajasvivinayak@gmail.com>
2021-05-13 00:00:19 +0530

    fix: minor readme changes

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -6,18 +6,29 @@ function About() {
             <div class="container">
                 <div class="row align-items-center my-5">
                     <div class="col-lg-3">
-                        <img class="img-fluid rounded mb-4 mb-lg-0" src="https://www.gifservice.fr/img/gif-vignette-small/9a6fa84a540e078db95db668a913898f/160441-video-games-brawlhalla-logo.gif" alt="" />
+                        <img
+                            class="img-fluid rounded mb-4 mb-lg-0"
+                            src="https://www.gifservice.fr/img/gif-vignette-small/9a6fa84a540e078db95db668a913898f/160441-video-games-brawlhalla-logo.gif"
+                            alt=""
+                        />
                     </div>
                     <div class="col-lg-5">
                         <h1 class="font-weight-light">About</h1>
                         <hr />
                         <p>
-                            Welcome to a Gold Pleb's passtime to track the ever-boucning elo between Gold and Platinum!
+                            Welcome to a Gold Pleb's hobby to track the ever-bouncing elo between Gold and Platinum!
                             Feel free to add tracker by adding your Brawlhalla username which will be periodically
-                            tracked (1v1: 15min, 2v2: 5min default interval); You can view the tracked username by
-                            clickng the added tracked username from homepage. Please send out you reviews/suggestions
-                            for features to: rajasvivinayak@gmail.com
-                            <br /><br /><a href="https://github.com/Rajasvi/Brawlhalla-Elo-Tracker-public" class="text-black display-6" ><i class="fab fa-github"></i></a>
+                            tracked (1v1: ever 15min, 2v2: every 5min default interval); You can view the elo rating
+                            graph for the tracked username by clickng the added username from homepage. Please send out
+                            your reviews/suggestions for features to: rajasvivinayak@gmail.com
+                            <br />
+                            <br />
+                            <a
+                                href="https://github.com/Rajasvi/Brawlhalla-Elo-Tracker-public"
+                                class="text-black display-6"
+                            >
+                                <i class="fab fa-github"></i>
+                            </a>
                         </p>
                     </div>
                 </div>
