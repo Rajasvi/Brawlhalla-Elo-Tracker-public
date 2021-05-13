48b9aac6c76f77c4652df40951445efcbbc0c945
Rajasvi <rajasvivinayak@gmail.com>
2021-05-14 00:23:35 +0530

    fix: Homepage layout changed to accomodate long list of trackers

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -24,19 +24,23 @@ function Home() {
             <div className="home">
                 <div class="container">
                     <div class="row align-items-center my-5">
-                        <div class="col-lg-7">
-                            <img
-                                class="img-fluid rounded mb-4 mb-lg-0"
-                                src="https://staticctf.akamaized.net/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/3fRtFxUJ37SKFvDWXF8mqI/4c08c54fc0136d9033b3680d7b9de7fc/brawlhalla-ubicom-page-meta-logo-960x540.png"
-                                alt=""
-                            />
+                        <div class="d-flex justify-content-center row">
+                            <div class="card  ">
+                                <img
+                                    class="card-img"
+                                    src="https://i.redd.it/6fvxi8wl3ts11.jpg"
+                                    alt="..."
+                                />
+                            </div>
+                            
                         </div>
-                        <div class="col-lg-5">
-                            <h1 class="font-weight-light">Current Trackers</h1>
-                            <hr />
-                            {trackerList.trackers.map((tracker) => (
+                    </div>
+                    <div class="row">
+                        <h1 class="font-weight-light">Current Trackers</h1>
+                        <hr />
+                        {trackerList.trackers.map((tracker) => (
+                            <div class="col-lg-2">
                                 <ul id={tracker.playerId}>
-                                    <div class="row">
                                     <Link
                                         id={tracker.playerId}
                                         to={{
@@ -47,10 +51,9 @@ function Home() {
                                     >
                                         {tracker.playerName}
                                     </Link>
-                                    </div>
                                 </ul>
-                            ))}
-                        </div>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
